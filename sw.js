03e80fa3892839ef189c35b19e5bd208e7e5d555
chiling222 <sajm222@gmail.com>
2026-07-19 10:43:53 +0800

    style: 桌面加寬與放大 v1.4

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "pw-shell-v4";
+const CACHE = "pw-shell-v5";
 const SHELL = ["./", "index.html", "css/style.css", "js/app.js", "manifest.webmanifest", "icons/icon.svg"];
 
 self.addEventListener("install", e => {
